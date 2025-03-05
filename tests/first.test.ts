5fad7c455055a0f57e7a0ef898695c60e71509ad
Максим Синицкий <m.sinitskiy@ddxfitness.local>
2025-03-06 00:14:12 +0300

    first commit

---
+++ tests/first.test.ts
@@ -0,0 +1,7 @@
+import { test } from "@playwright/test";
+
+test.describe("Описание группы теста", async () => {
+test("Я пишу тест", async () => {
+    console.log("Я написал тест! Ураа!");
+})
+});
