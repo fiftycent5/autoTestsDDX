7fef7b04dd3b9e3ad66416072e7dabdd6e4164f1
fiftycent5 <maksiniy27@gmail.com>
2025-03-17 17:05:53 +0300

    Урок 2

---
+++ tests/paymentPlans.test.ts
@@ -0,0 +1,24 @@
+import { expect, test } from "@playwright/test";
+import api from '../api.json';
+
+test.describe("API-тесты на получение списка тарифных планов", async () => {
+    test("[positive] получить список активных тарифных планов", async ({ request }) => {
+    const response = await request.get(
+    `${api.urls.base_url_api}${api.paths.paymentPlans}`,
+    {
+        headers: {
+            'Authoriazation': `${api.tokens.test}`
+        },
+        params: {
+            session_id: "1",
+            request_id: "2",
+            request_source: "crm",
+            is_active: true,
+            is_deleted: false
+        }
+    
+    }
+    );
+    expect(response.status()).toEqual(200);
+    });
+});
